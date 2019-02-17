95c89608341dd76b7e749f9973282c1996e22653
Andres Mena <ballerlikemike23@gmail.com>
2019-02-17 17:08:05 -0500

    Duplicating a Single Component

--- src/index.js
+++ src/index.js
@@ -23,6 +23,45 @@ const App = () => {
           </div>
         </div>
       </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+        <img src={faker.image.avatar()} alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">
+              Today at 6:00PM
+            </span>
+            <div className="text">
+              Nice blog post!
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+        <img src={faker.image.avatar()} alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">
+              Today at 6:00PM
+            </span>
+            <div className="text">
+              Nice blog post!
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
